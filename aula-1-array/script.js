5df47f2ac8418ecbd599b56f4f3eaf8d0f0829a0
Guilherme Carvalho <guicarvcontato@outlook.com>
2025-03-17 21:30:53 -0300

    exemplo de codigo aula 1 - arrays

---
+++ aula-1-array/script.js
@@ -0,0 +1,14 @@
+const listaDeCompras = ["Açucar", "Leite"]
+console.log(listaDeCompras)
+
+listaDeCompras[0] = 'Arroz'
+listaDeCompras[1] = 'Feijão'
+listaDeCompras[2] = 'Ovo'
+listaDeCompras[3] = 'Batata'
+listaDeCompras[4] = 'Tomate'
+listaDeCompras[5] = 'Bolacha'
+
+console.log(listaDeCompras)
+
+console.log(listaDeCompras[10]) // undefined
+
