2a0e5152424e2e4199f537f9c6395b2f62d1d17b
Guilherme Carvalho <guicarvcontato@outlook.com>
2025-03-18 22:24:32 -0300

    exemplo de codigo aula 4 - objetos

---
+++ aula-4-objetos/script.js
@@ -0,0 +1,11 @@
+const pessoa = {}
+
+pessoa.nome = 'Guilherme'
+pessoa.idade = '21'
+pessoa.profissao = 'desenvolvedor'
+
+console.log(pessoa)
+
+pessoa.tecnologias = ["javascript", "php", "css", "html"]
+console.log(pessoa)
+
