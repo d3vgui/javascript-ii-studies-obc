845a0a08b3cec14073210a78d6dabcadd40a8c7a
Guilherme Carvalho <guicarvcontato@outlook.com>
2025-03-24 08:03:16 -0300

    exemplo de codigo aula 8 - escopo

---
+++ aula-8-escopo/script.js
@@ -0,0 +1,47 @@
+let pokemon = 'Charmander'
+
+function evoluir(){
+  pokemon = 'Charmeleon'
+}
+
+// console.log(pokemon)
+// evoluir()
+// console.log(pokemon)
+
+function criarAnimal(){
+  let animal = 'gato'
+}
+
+// console.log(animal) // animal is not defined
+
+function avaliarNota(nota){
+  if (nota > 60){
+    var aprovado = true
+    let situacao = 'aprovado'
+  } else {
+    var aprovado = false
+    let situacao = 'reprovado'
+  }
+
+  console.log(nota)
+  console.log(aprovado)
+  console.log(situacao)
+}
+
+// avaliarNota(83)
+// avaliarNota(49)
+
+function olaMundo(){
+  var texto = 'Olá, mundo!'
+}
+
+// console.log(texto)
+
+console.log(nome)
+console.log(sobrenome)
+
+var nome = 'Guilherme'
+// let sobrenome = 'Carvalho'
+
+console.log(nome)
+console.log(sobrenome)
