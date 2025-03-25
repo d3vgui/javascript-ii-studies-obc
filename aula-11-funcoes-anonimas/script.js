2a788a416e8ff3144a20a69945c06d12a6817f98
Guilherme Carvalho <guicarvcontato@outlook.com>
2025-03-25 08:35:27 -0300

    exemplo de codigo aula 11 - funções anônimas

---
+++ aula-11-funcoes-anonimas/script.js
@@ -0,0 +1,17 @@
+function somar(a, b) {
+  return a + b
+}
+
+const operacao = somar
+console.log(operacao(4, 5))
+
+olaMundo()
+oiMundo()
+
+function olaMundo() {
+  console.log("Olá, mundo!")
+}
+
+const oiMundo = function () {
+  console.log("Oi, mundo!")
+}
